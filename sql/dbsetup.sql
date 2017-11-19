04267e061b4d2210c7ab5ab8d31de1ef8a9d958f
vagrant <vagrant@vagrant.vm>
2017-11-19 21:30:10 +0000

    Change database to not cascade as much. Start working on database.js commands

--- sql/dbsetup.sql
+++ sql/dbsetup.sql
@@ -1,51 +1,61 @@
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
+DROP TABLE IF EXISTS PlantOwnership;
+DROP TABLE IF EXISTS WaterEvent;
+DROP TABLE IF EXISTS Users;
+DROP TABLE IF EXISTS Plant;
+DROP TABLE IF EXISTS Location;
+
+
 /* Keeps track of each location on campus where a plant could be at */
 CREATE TABLE Location(
-	LocationID int NOT NULL AUTO_INCREMENT,
-	Building text,
-	Area text,
-	PRIMARY KEY(LocationID)
+	locationID int NOT NULL AUTO_INCREMENT,
+	building text NOT NULL,
+	area text NOT NULL, /*Should be a room number, a hallroom or etc. */
+	PRIMARY KEY(locationID)
 )ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 /*Keeps track of each plant in the database */
 CREATE TABLE Plant (
-  plantID int AUTO_INCREMENT,
+  plantID int NOT NULL AUTO_INCREMENT,
   locationID int,
   plantName text,
+  status enum('Alive', 'Dead', 'Gone') default 'Alive',
   PRIMARY KEY(plantID),
-  FOREIGN KEY(LocationID) REFERENCES Location(LocationID)
-
-)ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
-/*Tracks the time and date a user watered a plant */
-CREATE TABLE WaterEvent(
-	WaterID int AUTO_INCREMENT,
-	PlantID int,
-	TimeWatered TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-	PRIMARY KEY(WaterID),
-	FOREIGN KEY(PlantID) REFERENCES Plant(plantID)
+  FOREIGN KEY(locationID) REFERENCES Location(locationID) ON DELETE CASCADE
 )ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 /*The creditials of the user */
 CREATE TABLE Users(
-	UserID int,
-	Name text,
-	PhoneNumber char(10),
-	Role enum('User','Admin', 'Creator') default 'User',
-	PRIMARY KEY(UserID)
+	userID int NOT NULL,
+	name text NOT NULL,
+	phoneNumber char(10) NOT NULL,
+	role enum('User','Admin', 'Creator') default 'User',
+	PRIMARY KEY(userID)
+)ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Tracks the time and date a user watered a plant */
+CREATE TABLE WaterEvent(
+	waterID int NOT NULL AUTO_INCREMENT,
+	plantID int NOT NULL,
+	userID int NOT NULL,
+	timeWatered TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+	PRIMARY KEY(waterID, plantID),
+	FOREIGN KEY(plantID) REFERENCES Plant(plantID) ON DELETE CASCADE,
+	FOREIGN KEY(userID) REFERENCES Users(userID) ON DELETE CASCADE
 )ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 /*Relationship between a person and a plant */
 CREATE TABLE PlantOwnership(
-	UserID int,
-	plantID int,
-	PRIMARY KEY(UserID, plantID),
-	FOREIGN KEY(UserID) REFERENCES Users(UserID),
-	FOREIGN KEY(plantID) REFERENCES Plant(plantID)
+	userID int NOT NULL,
+	plantID int NOT NULL,
+	PRIMARY KEY(userID, plantID),
+	FOREIGN KEY(userID) REFERENCES Users(userID),
+	FOREIGN KEY(plantID) REFERENCES Plant(plantID) ON DELETE CASCADE
 )ENGINE=InnoDB DEFAULT CHARSET=latin1;
-	
+
+
 
 /* Insert test values */
 INSERT INTO Location(Building,Area)
@@ -59,11 +69,11 @@ INSERT INTO Plant(locationID,plantName)
 INSERT INTO Plant(locationID, plantName)
 	VALUES(2, "Botany");
 	
-INSERT INTO WaterEvent(PlantID)
-	VALUES(1);
-	
 INSERT INTO Users VALUES
 	(48755,"Maxwell Dulin","3605085170","User");
+	
+INSERT INTO WaterEvent(plantID,userID)
+	VALUES(1,48755);
 	
 INSERT INTO PlantOwnership VALUES
 	(48755,1);
--- src/controller/auth/database.js
+++ src/controller/auth/database.js
@@ -9,14 +9,24 @@ async function SQLConnect(testing) {
   });
 }
 
+//displays all of the tables
 async function showTables(connection) {
   const [results, fields] = await connection.query("show tables");
   return results;
 }
 
+//grabs all of the users
+async function getUsers(connection){
+	const [results,fields] = await connection.query("SELECT userID FROM Users");
+	return results;
+}
+
+//Runs the database functions
 async function database(request, response) {
   let connection = await SQLConnect();
-  let tables = await showTables(connection);
+  //let tables = await showTables(connection);
+  let users = await  getUsers(connection);
+  response.send(users)
   response.send(tables);
 }
 
