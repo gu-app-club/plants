90931203c33814ef130f407f02c49a24fa033b86
mdulin2 <mdulin2@zagmail.gonzaga.edu>
2017-11-21 22:41:02 -0800

    Add more functions to database.js

--- src/controller/auth/database.js
+++ src/controller/auth/database.js
@@ -9,6 +9,11 @@ async function SQLConnect(testing) {
   });
 }
 
+//generates a sudo random number
+function generateID(){
+	return Math.random().toString(36).substr(2,15);
+}
+
 //displays all of the tables
 async function showTables(connection) {
   const [results, fields] = await connection.query("show tables");
@@ -17,7 +22,7 @@ async function showTables(connection) {
 
 //grabs all of the users
 async function getUsers(connection){
-	const [results,fields] = await connection.query("SELECT userID FROM Users");
+	const [results,fields] = await connection.query("SELECT * FROM Users");
 	return results;
 }
 
@@ -34,15 +39,13 @@ async function getPlants(connection){
 async function addLocation(connection,building, areas){
 	building = building.toLowerCase(); //makes it so that the location table isn't scattered
 	areas = areas.toLowerCase();
-	//const [results,fields] = await connection.query("SELECT locationID FROM Location WHERE building = '" + building + "' AND area = '" + areas+ "'");
 	//prepared statement
 	const [results,fields] = await connection.query('SELECT locationID FROM Location WHERE building = ?  AND area = ?;',[building,areas]);
 
 	if(Object.keys(results).length == 0){ // checks for the location being in the database
-		//await connection.query("INSERT INTO Location(Building,Area) VALUES('"+building+"','"+ areas+"');");
 		 await connection.query("INSERT INTO Location(Building,Area) VALUES(?,?);",[building,areas]);
 	}
-	//const [result] = await connection.query("SELECT locationID FROM Location WHERE building = '" + building + "' AND area = '" + areas+ "'");
+
 	const [result] = await connection.query("SELECT locationID FROM Location WHERE building = ?  AND area = ?",[building,areas]);
 	//const [result] = await connection.query("Select * from Location");
 	return result;
@@ -60,19 +63,44 @@ async function addPlant(connection,building,areas,name,frequency,statusType){
 	
 	return id;
 }
+
+//Checks to see if the username is in use already
+//param: user-- the username being checked
+//returns: true if the username exists, false otherwise
+async function isUsername(connection,user){
+	const[key] = await connection.query("SELECT userID FROM Users WHERE userID = ?",[newID]);
+	if(Object.keys(key).length == 0){
+		return false;
+	}
+	return true;
+
+}
+
+//Adds the user to the database
+//param: connection--how connection to the database
+//param: username-- the unique ID of the user
+//param: name -- the actual name of the user
+//param: phoneNumber-- the phone number of the user
+//param: role-- the type of the user
+async function addUser(connection,username,name,phoneNumber, role){
+	var newID = "v7t8n781i9cyrs2";
+
+	const[result] = await connection.query("INSERT INTO Users(userID, name,phoneNumber,role) VALUES(?,?,?,?);",[username,name,phoneNumber,role]);
+}
+
 //Runs the database functions
 async function database(request, response) {
   let connection = await SQLConnect();
   //let tables = await showTables(connection);
-  //let users = await  getUsers(connection);
-  let plants = await getPlants(connection);
-  response.send(plants);
-  //response.send(users);
-  //response.send(tables);
-  //let location = await addLocation(connection,"herak","Room 321");
-  //let plant = await addPlant(connection,"PACCAR","Room 329", "planty!",5, "Alive");
-  
-  //response.send(plant);
+  let users = await  getUsers(connection);
+  //let plants = await getPlants(connection);
+ 
+  //response.send(plants);
+  response.send(users);
+  //console.log(generateID());
+  user = addUser(connection,"joeyblaze","jacob","360345170","Creator");
+  //response.send(user);
 }
+
 
 module.exports = database;
